f643129988c9cf827a3925fe000204b1c96aef4d
Amrit Sutar <sonuthesilentkiller@gmail.com>
2021-04-08 20:46:47 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var selectedRow = null;
- let count = 0;
+let count = 0;
+
+
 function validate() {
     var fullName = document.getElementById("fname");
     var degreeGrd = document.getElementById("degree");
@@ -7,14 +9,13 @@ function validate() {
     var dobVal = document.getElementById("dob");
     var mailId = document.getElementById("email");
     var phoneNo = document.getElementById("phone");
-    var studentId = document.getElementById("student");
 
     // ReGex Check
-    var regExp = /^[A-Za-z ]+$/;
+    var regExp = /^[A-Za-z. ]+$/;
     var dobCheck = /^[0-9]+\-[0-9]+\-[0-9]+$/;
     var mailCheck = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    var phoneCheck = / ^[0-9]{10}$/;
-    
+    var phoneCheck = /^[0-9]{10}$/;
+
 
     if (regExp.test(fullName.value)) {
         document.getElementById("nameError").innerHTML = " ";
@@ -55,26 +56,24 @@ function validate() {
     if (phoneCheck.test(phoneNo.value)) {
         document.getElementById("phoneError").innerHTML = " ";
     } else {
-        document.getElementById("phoneError").innerHTML =
-            " ** Phone no is Invalid ";
+        document.getElementById("phoneError").innerHTML = " ** Phone no is Invalid ";
         return false;
     }
 
-  
+
 }
 
 
+
 function onFormSubmit() {
+
     if (readFormData()) {
         var formData = readFormData();
-        if (selectedRow == null) 
-        {
+        if (selectedRow == null) {
             insertNewRecord(formData);
-        }
-        else
-        {
+        } else {
             updateRecord(formData);
-        } 
+        }
         resetForm();
     }
 }
@@ -88,7 +87,6 @@ function readFormData() {
     formData["dob"] = document.getElementById("dob").value;
     formData["email"] = document.getElementById("email").value;
     formData["phone"] = document.getElementById("phone").value;
-    formData["student"] = document.getElementById("student").value;
     return formData;
 }
 
@@ -99,7 +97,7 @@ function rand() {
 function insertNewRecord(data) {
     var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
-    
+
     cell1 = newRow.insertCell(0);
     cell1.innerHTML = data.fname;
 
@@ -134,7 +132,6 @@ function resetForm() {
     document.getElementById("dob").value = "";
     document.getElementById("email").value = "";
     document.getElementById("phone").value = "";
-    document.getElementById("student").value = "";
     selectedRow = null;
 }
 
@@ -146,7 +143,6 @@ function onEdit(td) {
     document.getElementById("dob").value = selectedRow.cells[3].innerHTML;
     document.getElementById("email").value = selectedRow.cells[4].innerHTML;
     document.getElementById("phone").value = selectedRow.cells[5].innerHTML;
-    document.getElementById("student").value = selectedRow.cells[6].innerHTML;
 }
 
 function updateRecord(formData) {
@@ -156,7 +152,6 @@ function updateRecord(formData) {
     selectedRow.cells[3].innerHTML = formData.dob;
     selectedRow.cells[4].innerHTML = formData.email;
     selectedRow.cells[5].innerHTML = formData.phone;
-    selectedRow.cells[6].innerHTML = formData.student;
 }
 
 function onDelete(ta) {
@@ -166,3 +161,85 @@ function onDelete(ta) {
         resetForm();
     }
 }
+
+function storeData() {
+
+     var new_data = [
+        {
+        fname: document.getElementById("fname").value,
+        degree: document.getElementById("degree").value,
+        subDegree: document.getElementById("subDegree").value,
+        dob: document.getElementById("dob").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+      }
+    ]; 
+
+    // if there is nothing saved at the start then save an empty array. 
+    if(localStorage.getItem('data') == null) {
+        localStorage.setItem('data' , '[]');
+    }
+
+    // get the old data & slap it on to the new data.
+    var old_data = JSON.parse(localStorage.getItem('data'));
+    old_data.push(new_data);
+
+    // saving the old data + new data to our local storage.
+    localStorage.setItem('data' , JSON.stringify(old_data));
+}
+
+function viewData() {
+    if(localStorage.getItem('data') != null) {
+        console.log(JSON.parse(localStorage.getItem('data')));
+    }
+}
+
+
+// Table sort based on name alphabetical order.
+
+var th = document.getElementsByTagName("th");
+
+for (let c = 0; c < th.length; c++) {
+    th[c].addEventListener("click", item(c));
+}
+
+function item(c) {
+    return function () {
+        console.log(c);
+        sortTable(c);
+    };
+}
+
+function sortTable(c) {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("employeeList");
+    switching = true;
+    /*Make a loop that will continue until no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /*Loop through all table rows (except the first, which contains table headers):*/
+        for (i = 1; i < rows.length - 1; i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare, one from current row and one from the next:*/
+            x = rows[i].getElementsByTagName("TD")[c];
+            y = rows[i + 1].getElementsByTagName("TD")[c];
+            //check if the two rows should switch place:
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                //if so, mark as a switch and break the loop:
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+}
+
+/* End of name sorting */
+
